bfcd3bdf42581bc889b04f4a25860ca7e9be9a8b
waseemfarhat <141494576+waseemfarhat@users.noreply.github.com>
2023-12-03 11:43:06 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import Investment from "./Components/FormHandler";
+import Results from "./Components/Result.js";
+function App() {
+  
+  return (
+<div>
+<Investment />
+</div>
+  )
+}
+
+export default App;
